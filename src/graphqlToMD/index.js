145597878b92bff2df243e1a5450b8b7724032b0
abaena <abaena@xmltravelgate.com>
2018-07-25 12:22:20 +0200

    fixed major bug with urls

--- src/graphqlToMD/index.js
+++ src/graphqlToMD/index.js
@@ -9,24 +9,37 @@ function init() {
     config.MD_DATA = JSON.parse(data);
   });
 
-  fsex.emptyDirSync(config.getLocation());
-  fsex.emptyDirSync(config.getLocation() + '/objects');
-  fsex.emptyDirSync(config.getLocation() + '/schema');
-  fsex.emptyDirSync(config.getLocation() + '/inputobjects');
-  fsex.emptyDirSync(config.getLocation() + '/interfaces');
-  fsex.emptyDirSync(config.getLocation() + '/enums');
-  fsex.emptyDirSync(config.getLocation() + '/scalars');
+  const loc = config.getLocation();
 
-  fsex.readFile(__dirname + '/../tmp/introspection.json', (err, data) => {
-    if (err) throw err;
-    const parsedData = JSON.parse(data);
-    bar.tick();
-    if (config.SCHEMA_OPTIONS.length) {
-      parsedData.__schema.queryType = config.SCHEMA_OPTIONS[0];
-      parsedData.__schema.mutationType = config.SCHEMA_OPTIONS[1];
-    }
+  process.umask(777)
 
-    renderToHugo(parsedData);
+  fsex.emptyDir(loc, err => {
+    if (err) return console.error(err);
+    setTimeout(_ => {
+      // fsex.chmodSync(loc, 0777);
+      fsex.emptyDirSync(`${loc}/objects`);
+      fsex.emptyDirSync(`${loc}/schema`);
+      fsex.emptyDirSync(`${loc}/inputobjects`);
+      fsex.emptyDirSync(`${loc}/interfaces`);
+      fsex.emptyDirSync(`${loc}/enums`);
+      fsex.emptyDirSync(`${loc}/scalars`);
+
+      fsex.readFile(
+        __dirname + '/../tmp/introspection.json',
+        (err, data) => {
+          if (err) throw err;
+          const parsedData = JSON.parse(data);
+          bar.tick();
+          if (config.SCHEMA_OPTIONS.length) {
+            parsedData.__schema.queryType = config.SCHEMA_OPTIONS[0];
+            parsedData.__schema.mutationType = config.SCHEMA_OPTIONS[1];
+          }
+
+          renderToHugo(parsedData);
+        },
+        1000
+      );
+    });
   });
 }
 
